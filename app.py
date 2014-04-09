2973107ae3583e61e789259927fcd0af953c55f2
Alexey Ivanov <aivanov@linkedin.com>
2014-04-09 00:40:17 -0700

    app: added ability to serve public keys

--- app.py
+++ app.py
@@ -2,14 +2,27 @@
 
 import os
 import random
-from flask import Flask, Response
+from flask import Flask, Response, abort
 
 app = Flask(__name__)
 quotes = tuple(open('quotes.txt'))
 
-@app.route('/<path:filename>')
-@app.route('/')
-def index(filename='/'):
+@app.route('/_keys/<path:key_id>.pub.asc')
+def keys(key_id):
+    try:
+        key_str = hex(int(key_id, base=16))
+    except ValueError:
+        return abort(403)
+    filename = '_keys/{0}.pub.asc'.format(key_str)
+    try:
+        with open(filename) as fp:
+            return Response(fp.read(), mimetype='text/plain')
+    except (IOError, OSError):
+        return abort(404)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return Response(random.choice(quotes), mimetype='text/plain')
 
 if __name__ == '__main__':
